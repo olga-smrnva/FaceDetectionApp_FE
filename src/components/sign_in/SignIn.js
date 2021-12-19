54e67113499f2072c881a594157a148de5f66093
Olga Smirnova <smrnva.olga@gmail.com>
2021-12-19 12:21:45 +0200

    Adding new server path (heroku)

--- src/components/sign_in/SignIn.js
+++ src/components/sign_in/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 	};
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://afternoon-cliffs-28203.herokuapp.com/signin', {
 			method: 'post',
 			headers : {'Content-type': 'application/json'},
 			body: JSON.stringify({
